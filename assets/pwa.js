cd5553e945b5758e8df345ec42023a000a6f9de5
oohassets <oohassetscoop@gmail.com>
2025-11-10 15:59:00 +0300

    Update pwa.js

--- assets/pwa.js
+++ assets/pwa.js
@@ -1,4 +1,7 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js');
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(reg => console.log('Service Worker Registered:', reg))
+      .catch(err => console.error('SW Registration Failed:', err));
+  });
 }
-
