a158780a55b9d0789fa920cb01e2ea8c6d392256
oohassets <oohassetscoop@gmail.com>
2025-11-10 19:53:04 +0300

    Update ui.js

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,1 +1,12 @@
+window.addEventListener("load", () => {
+  const spl = document.getElementById("splash-screen");
+  const logo = document.getElementById("splash-logo");
+
+  logo.classList.add("visible");
+
+  setTimeout(() => {
+    spl.classList.add("fade-out");
+    setTimeout(() => spl.remove(), 800);
+  }, 2200);
+});
 
