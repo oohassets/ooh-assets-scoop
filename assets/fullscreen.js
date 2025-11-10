c7b1c42e83a268da957351cde1594085e3ad65a2
oohassets <oohassetscoop@gmail.com>
2025-11-10 14:58:02 +0300

    Update fullscreen.js

--- assets/fullscreen.js
+++ assets/fullscreen.js
@@ -1,1 +1,33 @@
+const wrapper = document.getElementById("mapWrapper");
+const inventoryWrapper = document.getElementById("inventoryWrapper");
+
+document.getElementById("fsBtn").onclick = async () => {
+  if (!document.fullscreenElement) {
+    await wrapper.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+};
+
+document.getElementById("showInventoryBtn").onclick = async () => {
+  if (!document.fullscreenElement) {
+    inventoryWrapper.style.display = "block";
+    try {
+      await inventoryWrapper.requestFullscreen();
+    } catch {
+      inventoryWrapper.classList.add("fullscreen-wrapper");
+      document.body.classList.add("no-scroll");
+    }
+  } else {
+    document.exitFullscreen();
+  }
+};
+
+document.addEventListener("fullscreenchange", () => {
+  if (document.fullscreenElement !== inventoryWrapper) {
+    inventoryWrapper.style.display = "none";
+    inventoryWrapper.classList.remove("fullscreen-wrapper");
+    document.body.classList.remove("no-scroll");
+  }
+});
 
