a152b39f8b7d4b90e1257ddf4fc02d0d3a6607d7
oohassets <oohassetscoop@gmail.com>
2025-11-10 19:50:00 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
+import { auth } from "../../firebase/firebase.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { loadMapLinks } from "./map.js";
+import { loadInventory } from "./inventory.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const logoutText = document.getElementById("logoutText");
+
+  onAuthStateChanged(auth, user => {
+    if (!user) {
+      window.location.href = "login.html";
+    } else {
+      document.querySelector(".container").style.display = "block";
+      loadMapLinks();
+      loadInventory();
+    }
+  });
+
+  logoutText.addEventListener("click", () => {
+    signOut(auth).then(() => {
+      window.location.href = "login.html";
+    });
+  });
+});
 
