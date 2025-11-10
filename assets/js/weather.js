a6d588068692cc22286b9baf04e2bee21e2778c2
oohassets <oohassetscoop@gmail.com>
2025-11-10 19:52:26 +0300

    Update weather.js

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,1 +1,18 @@
+export async function fetchWeather() {
+  try {
+    const res = await fetch(
+      "https://api.open-meteo.com/v1/forecast?latitude=25.2854&longitude=51.5310&current=temperature_2m&timezone=auto"
+    );
+
+    const data = await res.json();
+    const temp = data.current.temperature_2m;
+
+    document.getElementById("weatherTemp").textContent = `${temp}°C`;
+  } catch (error) {
+    document.getElementById("weatherTemp").textContent = "N/A";
+  }
+}
+
+setInterval(fetchWeather, 600000);
+fetchWeather();
 
