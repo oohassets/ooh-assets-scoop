25db8c8436d6ce34fc9ac474d7ec1f04761d93b7
oohassets <oohassetscoop@gmail.com>
2025-11-11 08:18:44 +0300

    Delete assets/pwa.js

--- assets/pwa.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then(reg => console.log('Service Worker Registered:', reg))
-      .catch(err => console.error('SW Registration Failed:', err));
-  });
-}
