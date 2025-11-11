0e5abe723cde9c7017193d40b9f781d29e323cfd
oohassets <oohassetscoop@gmail.com>
2025-11-11 20:33:21 +0300

    Update inventory.js

--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -2,8 +2,8 @@ import { db } from "../../firebase/firebase.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 export async function loadInventory() {
-  const contentInventoryBtn = doc(db, "inventory", "contentInventory");
-  const snap = await getDoc(contentInventoryBtn);
+  const inventoryDoc = doc(db, "inventory", "contentInventory");
+  const snap = await getDoc(inventoryDoc);
 
   if (snap.exists()) {
     document.getElementById("inventoryIframe").src = snap.data().iframeUrl;
@@ -11,3 +11,6 @@ export async function loadInventory() {
     console.error("Inventory document not found in Firestore");
   }
 }
+
+document.getElementById("contentInventoryBtn")
+  .addEventListener("click", loadInventory);
