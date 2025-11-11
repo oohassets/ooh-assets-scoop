0cb16db8e1f1ef2aa4ad54a9a38c20408f3f4c8d
oohassets <oohassetscoop@gmail.com>
2025-11-11 14:39:47 +0300

    Update fullscreen.js

--- assets/js/fullscreen.js
+++ assets/js/fullscreen.js
@@ -1,1 +1,16 @@
+export function initFullscreen() {
+  const fsBtn = document.getElementById("fsBtn");
+  const wrapper = document.getElementById("mapWrapper");
 
+  fsBtn.addEventListener("click", async () => {
+    try {
+      if (!document.fullscreenElement) {
+        await wrapper.requestFullscreen();
+      } else {
+        await document.exitFullscreen();
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  });
+}
