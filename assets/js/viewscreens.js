ce76c197caf02a2c3825d0b17d4748974a4b6ece
oohassets <oohassetscoop@gmail.com>
2025-11-12 13:45:51 +0300

    Update viewscreens.js

--- assets/js/viewscreens.js
+++ assets/js/viewscreens.js
@@ -1,1 +1,25 @@
+function isInStandaloneMode() {
+  return window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone === true;
+}
+
+const viewScreensBtn = document.getElementById('viewScreensBtn');
+viewScreensBtn.addEventListener('click', () => {
+  const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+  if (isInStandaloneMode()) {
+    window.location.href = "asset-digital-content.html";
+  } else if (isMobile) {
+    const fullscreenWin = window.open(
+      "asset-digital-content.html",
+      "_blank",
+      "toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,fullscreen=yes,width=" +
+        screen.availWidth +
+        ",height=" +
+        screen.availHeight
+    );
+    if (fullscreenWin) fullscreenWin.focus();
+  } else {
+    window.open("asset-digital-content.html", "_blank");
+  }
+});
 
