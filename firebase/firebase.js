9583f3ccc645661f34a3787d0fa48b8b51b88bac
oohassets <oohassetscoop@gmail.com>
2025-11-17 14:42:29 +0300

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -16,4 +16,5 @@ export const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const rtdb = getDatabase(app);
 
