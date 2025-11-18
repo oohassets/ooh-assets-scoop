f2b9da0132e3bf4a39d39cc6f6105c3a4d964c5b
oohassets <oohassetscoop@gmail.com>
2025-11-18 08:14:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // ===== SCOOP OOH ASSETS - SERVICE WORKER =====
-const CACHE_NAME = 'scoop-ooh-cache-v53.10';
+const CACHE_NAME = 'scoop-ooh-cache-v54';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
