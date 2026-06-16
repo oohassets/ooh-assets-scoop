6aa9930b7a0b8c883a03aebea5cabb61abba5b3f
oohassets <oohassetscoop@gmail.com>
2026-06-16 10:54:18 +0300

    fix: Update service worker cache version to v114; adjust styles in main.html

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ importScripts("https://www.gstatic.com/firebasejs/11.0.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "   ",
+  apiKey: "AIzaSyA4amK6CZuiU3_Nfaw4OLD17BqWrX0VYAA",
   authDomain: "scoopassets.firebaseapp.com",
   projectId: "scoopassets",
   storageBucket: "scoopassets.firebasestorage.app",
